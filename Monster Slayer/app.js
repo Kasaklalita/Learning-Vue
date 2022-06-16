25c0046031f87e67ccef627a7282a4d101b08785
Veniamin Polienko <kasaklalita@mail.ru>
2022-06-16 14:13:40 +0300

    Finishing the core functionality

--- Monster Slayer/app.js
+++ Monster Slayer/app.js
@@ -7,14 +7,41 @@ const app = Vue.createApp({
 		return {
 			playerHealth: 100,
 			monsterHealth: 100,
-			currentRound: 0
+			currentRound: 0,
+			winner: null
 		};
+	},
+	watch: {
+		playerHealth(value) {
+			if (value <= 0 && this.monsterHealth <= 0) {
+				//A draw
+				this.winner = 'draw';
+			} else if (value <= 0) {
+				//Player lost
+				this.winner = 'monster';
+			}
+		},
+		monsterHealth(value) {
+			if (value <= 0 && this.playerHealth <= 0) {
+				//A draw
+				this.winner = 'draw';
+			} else if (value <= 0) {
+				//Monster lost
+				this.winner = 'player';
+			}
+		}
 	},
 	computed: {
 		monsterBarStyles() {
+			if (this.monsterHealth < 0) {
+				return {width: '0%'};
+			}
 			return {width: this.monsterHealth + '%'}
 		},
 		playerBarStyles() {
+			if (this.playerHealth < 0) {
+				return {width: '0%'};
+			}
 			return {width: this.playerHealth + '%'}
 		},
 		mayUseSpecialAttack() {
@@ -22,6 +49,12 @@ const app = Vue.createApp({
 		}
 	},
 	methods: {
+		startGame() {
+			this.playerHealth = 100;
+			this.monsterHealth = 100;
+			this.currentRound = 0;
+			this.winner = null;
+		},
 		attackMonster() {
 			this.currentRound++;
 			const attackValue = getRandomValue(5, 12);
@@ -47,6 +80,9 @@ const app = Vue.createApp({
 				this.playerHealth += healValue;
 			}
 			this.attackPlayer();
+		},
+		surrender() {
+			this.winner = 'monster';
 		}
 	}
 });
