fd19f47eea24244ff5d8a67a4b0571de132ba446
Veniamin Polienko <kasaklalita@mail.ru>
2022-06-16 14:34:08 +0300

    Adding a battle log

--- Monster Slayer/app.js
+++ Monster Slayer/app.js
@@ -8,7 +8,8 @@ const app = Vue.createApp({
 			playerHealth: 100,
 			monsterHealth: 100,
 			currentRound: 0,
-			winner: null
+			winner: null,
+			logMessages: []
 		};
 	},
 	watch: {
@@ -54,21 +55,25 @@ const app = Vue.createApp({
 			this.monsterHealth = 100;
 			this.currentRound = 0;
 			this.winner = null;
+			this.logMessages = [];
 		},
 		attackMonster() {
 			this.currentRound++;
 			const attackValue = getRandomValue(5, 12);
 			this.monsterHealth -=  attackValue;
+			this.addLogMessage('player', 'attack', attackValue);
 			this.attackPlayer();
 		},
 		attackPlayer() {
 			const attackValue = getRandomValue(8, 15);
 			this.playerHealth -= attackValue;
+			this.addLogMessage('monster', 'player', attackValue);
 		},
 		specialAttackMonster() {
 			this.currentRound++;
 			const attackValue = getRandomValue(10, 25);
 			this.monsterHealth -=  attackValue;
+			this.addLogMessage('player', 'special attack', attackValue);
 			this.attackPlayer();
 		},
 		healPlayer() {
@@ -79,10 +84,18 @@ const app = Vue.createApp({
 			} else {
 				this.playerHealth += healValue;
 			}
+			this.addLogMessage('player', 'heal', healValue);
 			this.attackPlayer();
 		},
 		surrender() {
 			this.winner = 'monster';
+		},
+		addLogMessage(who, what, value) {
+			this.logMessages.unshift({
+				actionBy: who,
+				actionType: what,
+				actionValue: value
+			});
 		}
 	}
 });
