cf66028bf47e3e0553377cb988521c3dae3f2828
Veniamin Polienko <kasaklalita@mail.ru>
2022-06-16 19:30:58 +0300

    Vue`s reactivity

---
+++ Introduction to Vue`s Reactivity/app.js
@@ -0,0 +1,46 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      //this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+    },
+  },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  }
+});
+app.mount('#app');
+
+const app2= Vue.createApp({
+  template: `{{ favoriteMeal }}`,
+  data() {
+    return {
+      favoriteMeal: 'Pizza'
+    }
+  }
+});
+app2.mount('#app2');
