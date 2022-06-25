fd2a7b3d957835113c60f5ca1ca8913af98606b4
Veniamin Polienko <kasaklalita@mail.ru>
2022-06-25 20:04:31 +0300

    Dynamic components and keeping them alive

--- Diving Deeper Into Components/src/App.vue
+++ Diving Deeper Into Components/src/App.vue
@@ -1,34 +1,46 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals  #default="slotProps">
-      <h2>{{slotProps.item}}</h2>
-      <p>{{ slotProps['another-prop'] }}</p>
-    </course-goals>
+<!--    <badge-list></badge-list>-->
+<!--    <user-info-->
+<!--      :full-name="activeUser.name"-->
+<!--      :info-text="activeUser.description"-->
+<!--      :role="activeUser.role"-->
+<!--    ></user-info>-->
+<!--    <course-goals  #default="slotProps">-->
+<!--      <h2>{{slotProps.item}}</h2>-->
+<!--      <p>{{ slotProps.another }}</p>-->
+<!--    </course-goals>-->
+    <button @click="setSelectedComponent('active-goals')">Active Goal</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+<!--    <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>-->
+<!--    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>-->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
   import TheHeader from "./components/TheHeader";
-  import BadgeList from './components/BadgeList.vue';
-  import UserInfo from './components/UserInfo.vue';
-  import CourseGoals from "./components/CourseGoals";
+  // import BadgeList from './components/BadgeList.vue';
+  // import UserInfo from './components/UserInfo.vue';
+  // import CourseGoals from "./components/CourseGoals.vue";
+  import ActiveGoals from "./components/ActiveGoals.vue";
+  import ManageGoals from "./components/ManageGoals.vue";
 
   export default {
     components: {
       TheHeader,
-      BadgeList,
-      UserInfo,
-      CourseGoals
+      // BadgeList,
+      // UserInfo,
+      // CourseGoals,
+      ActiveGoals,
+      ManageGoals
     },
     data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -36,6 +48,11 @@
       },
     };
   },
+    methods: {
+      setSelectedComponent(cmp) {
+        this.selectedComponent = cmp;
+      }
+    }
 };
 </script>
 
--- Diving Deeper Into Components/src/components/CourseGoals.vue
+++ Diving Deeper Into Components/src/components/CourseGoals.vue
@@ -1,7 +1,7 @@
 <template>
 	<ul>
 		<li v-for="goal in goals" :key="goal">
-			<slot :item="goal" another-prop="..."></slot>
+			<slot :item="goal" another="..."></slot>
 		</li>
 	</ul>
 </template>
