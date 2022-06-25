c9a78052d3689d8906e40bf62723a6ccaf427ccc
Veniamin Polienko <kasaklalita@mail.ru>
2022-06-25 14:05:09 +0300

    Defining & Validating cusrom events

--- More on components/src/components/FriendContact.vue
+++ More on components/src/components/FriendContact.vue
@@ -38,6 +38,17 @@
 				}
 			}
 		},
+		emits: ['toggle-favorite'],
+		// emits: {
+		// 	'toggle-favorite': function(id) {
+		// 		if (id) {
+		// 			return true;
+		// 		} else {
+		// 			console.warn('Id is missing');
+		// 			return false;
+		// 		}
+		// 	}
+		// },
 		data() {
 			return {
 				detailsAreVisible: false
