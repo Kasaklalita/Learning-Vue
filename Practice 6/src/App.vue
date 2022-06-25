a08cc5b9d9d02babd87a3e76a7519e506aa74559
Veniamin Polienko <kasaklalita@mail.ru>
2022-06-25 15:22:17 +0300

    Completed practice 6

---
+++ Practice 6/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<active-user
+		:name="user.name"
+		:age="user.age"
+	></active-user>
+	<user-data
+		@submit-user="submitUser"
+	></user-data>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				user: {
+					name: 'Veniamin',
+					age: 18
+				}
+			};
+		},
+		methods: {
+			submitUser(name, age) {
+				this.user = {
+					name: name,
+					age: +age
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Practice 6/src/components/UserData.vue
@@ -0,0 +1,34 @@
+<template>
+	<form action="" @submit.prevent="submitUser">
+		<div>
+			<label>Name: </label>
+			<input type="text" v-model="enteredName">
+		</div>
+		<div>
+			<label>Age: </label>
+			<input type="text" v-model="enteredAge">
+		</div>
+		<button>Submit</button>
+	</form>
+</template>
+
+<script>
+	export default {
+		emits: ['submit-user'],
+		data() {
+			return {
+				enteredName: '',
+				enteredAge: ''
+			};
+		},
+		methods: {
+			submitUser() {
+				this.$emit('submit-user', this.enteredName, this.enteredAge);
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
