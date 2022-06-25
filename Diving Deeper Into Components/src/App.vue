83baa1d4fcbc29c26b4f84178fa64570471ae07e
Veniamin Polienko <kasaklalita@mail.ru>
2022-06-25 19:50:15 +0300

    More on slots & scoped slots

--- Diving Deeper Into Components/src/App.vue
+++ Diving Deeper Into Components/src/App.vue
@@ -7,6 +7,10 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals  #default="slotProps">
+      <h2>{{slotProps.item}}</h2>
+      <p>{{ slotProps['another-prop'] }}</p>
+    </course-goals>
   </div>
 </template>
 
@@ -14,12 +18,14 @@
   import TheHeader from "./components/TheHeader";
   import BadgeList from './components/BadgeList.vue';
   import UserInfo from './components/UserInfo.vue';
+  import CourseGoals from "./components/CourseGoals";
 
   export default {
     components: {
       TheHeader,
       BadgeList,
-      UserInfo
+      UserInfo,
+      CourseGoals
     },
     data() {
     return {
--- Diving Deeper Into Components/src/components/BaseCard.vue
+++ Diving Deeper Into Components/src/components/BaseCard.vue
@@ -1,7 +1,9 @@
 <template>
 	<div>
-		<header>
-			<slot name="header"></slot>
+		<header v-if="$slots.header">
+			<slot name="header">
+<!--				<h2>The Default</h2>-->
+			</slot>
 		</header>
 		<slot></slot>
 	</div>
@@ -9,6 +11,9 @@
 
 <script>
 	export default {
+		mounted() {
+			console.log(this.$slots.header);
+		}
 	}
 </script>
 
