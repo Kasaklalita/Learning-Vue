01b0ecfb5a2663e772a65daedd439d5aa376b5f5
Veniamin Polienko <kasaklalita@mail.ru>
2022-06-26 18:55:05 +0300

    GET and POST requests

---
+++ Sending Http Requests/src/components/survey/UserExperiences.vue
@@ -0,0 +1,60 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: []
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch('https://vue-http-demo-8e969-default-rtdb.firebaseio.com/surveys.json').then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+      }).then(data => {
+        const results = [];
+        for (const i in data) {
+          results.push({
+            id: i,
+            name: data[i].name,
+            rating: data[i].rating
+          });
+        }
+        this.results = results;
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
