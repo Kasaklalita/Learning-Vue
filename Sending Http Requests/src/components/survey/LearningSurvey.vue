7c023be0ff753b3e924efefeee72e3fd1b02b1ff
Veniamin Polienko <kasaklalita@mail.ru>
2022-06-26 20:49:50 +0300

    Handling errors

--- Sending Http Requests/src/components/survey/LearningSurvey.vue
+++ Sending Http Requests/src/components/survey/LearningSurvey.vue
@@ -29,6 +29,7 @@
         <p
           v-if="invalidInput"
         >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="error">{{error}}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -44,6 +45,7 @@ export default {
       enteredName: '',
       chosenRating: null,
       invalidInput: false,
+      error: null
     };
   },
   emits: ['survey-submit'],
@@ -60,6 +62,7 @@ export default {
         rating: this.chosenRating,
       });
 
+      this.error = null
       fetch('https://vue-http-demo-8e969-default-rtdb.firebaseio.com/surveys.json', {
         method: 'POST',
         headers: {
@@ -69,6 +72,15 @@ export default {
           name: this.enteredName,
           rating: this.chosenRating
         })
+      }).then(responce => {
+        if (responce.ok) {
+          // .
+        } else {
+          throw new Error('Could not save data!');
+        }
+      }).catch(error => {
+        console.log(error);
+        this.error = error.message;
       });
 
       this.enteredName = '';
--- Sending Http Requests/src/components/survey/UserExperiences.vue
+++ Sending Http Requests/src/components/survey/UserExperiences.vue
@@ -5,7 +5,10 @@
       <div>
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{error}}</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found. Start adding some survey results first.</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -26,16 +29,21 @@ export default {
   },
   data() {
     return {
-      results: []
+      results: [],
+      isLoading: false,
+      error: null
     };
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
+      this.error = null;
       fetch('https://vue-http-demo-8e969-default-rtdb.firebaseio.com/surveys.json').then(response => {
         if (response.ok) {
           return response.json();
         }
       }).then(data => {
+        this.isLoading = false;
         const results = [];
         for (const i in data) {
           results.push({
@@ -45,8 +53,15 @@ export default {
           });
         }
         this.results = results;
+      }).catch((error) => {
+        this.isLoading = false;
+        console.log(error);
+        this.error = 'Failed to fetch data - please try again later!'
       });
     }
+  },
+  mounted() {
+    this.loadExperiences();
   }
 };
 </script>
