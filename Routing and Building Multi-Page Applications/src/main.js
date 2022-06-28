974a18e557b37d828650b5f10c0df4e458914a9a
Veniamin Polienko <kasaklalita@mail.ru>
2022-06-28 22:00:25 +0300

    Using nested routes

--- Routing and Building Multi-Page Applications/src/main.js
+++ Routing and Building Multi-Page Applications/src/main.js
@@ -17,16 +17,18 @@ const router = createRouter({
 		{
 			path: '/teams',
 			component: TeamsList,
+			children: [
+				{
+					path: ':teamId',
+					component: TeamMembers,
+					props: true
+				}
+			]
 			//alias: '/'
 		},
 		{
 			path: '/users',
 			component: UsersList
-		},
-		{
-			path: '/teams/:teamId',
-			component: TeamMembers,
-			props: true
 		},
 		{
 			path: '/:notFound(.*)',
