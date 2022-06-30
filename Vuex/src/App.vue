62847b655b1f50d419777099c329b88422002aa9
Veniamin Polienko <kasaklalita@mail.ru>
2022-06-30 15:04:48 +0300

    Introducting mutations & passing data to mutations

--- Vuex/src/App.vue
+++ Vuex/src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <base-container title="Vuex">
     <the-counter></the-counter>
-    <button @click="addOne">Add 1</button>
+    <button @click="addOne">Add 10</button>
+    <change-counter></change-counter>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from "./components/TheCounter";
+import ChangeCounter from "./components/ChangeCounter";
 
 export default {
   components: {
     BaseContainer,
-    TheCounter
+    TheCounter,
+    ChangeCounter
   },
   methods: {
     addOne() {
-      this.$store.state.counter++;
+      // this.$store.commit('increase', {value: 10});
+      this.$store.commit({
+        type: 'increase',
+        value: 10
+      });
     }
   }
 };
---
+++ Vuex/src/components/ChangeCounter.vue
@@ -0,0 +1,17 @@
+<template>
+	<button @click="addOne">Add 2</button>
+</template>
+
+<script>
+	export default {
+		methods: {
+			addOne() {
+				this.$store.commit('increase', {value: 2});
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
