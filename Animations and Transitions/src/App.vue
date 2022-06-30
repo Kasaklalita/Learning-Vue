e2616d0943749221bb6f15ee9742e5d963072ab4
Veniamin Polienko <kasaklalita@mail.ru>
2022-06-30 13:30:41 +0300

    Using transition events & building JavaScript transitions

--- Animations and Transitions/src/App.vue
+++ Animations and Transitions/src/App.vue
@@ -4,7 +4,16 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition name="para">
+    <transition
+      name="para"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @enter-cancelled="enterCancelled"
+      @leave-cancelled="leaveCancelled">
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
@@ -31,7 +40,9 @@ export default {
       animatedBlock: false,
       dialogIsVisible: false,
       paraIsVisible: false,
-      usersAreVisible: false
+      usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null
     };
   },
   methods: {
@@ -52,6 +63,58 @@ export default {
     },
     hideUsers() {
       this.usersAreVisible = false;
+    },
+    beforeEnter(el) {
+      console.log('beforeEnter');
+      console.log(el);
+      el.style.opacity = 0;
+    },
+    enter(el, done) {
+      console.log('enter');
+      console.log(el);
+      let round = 1;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterEnter(el) {
+      console.log('afterEnter');
+      console.log(el);
+    },
+    beforeLeave(el) {
+      console.log('beforeLeave');
+      console.log(el);
+      el.style.opacity = 1;
+    },
+    leave(el, done) {
+      console.log('leave');
+      console.log(el);
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterLeave(el) {
+      console.log('afterLeave');
+      console.log(el);
+    },
+    enterCancelled(el) {
+      clearInterval(this.enterInterval);
+      console.log(el);
+    },
+    leaveCancelled(el) {
+      clearInterval(this.leaveInterval);
+      console.log(el);
     }
   },
 };
@@ -102,36 +165,6 @@ button:active {
   .animate {
     /*transform: translateX(-150px);*/
     animation: slide-scale 0.3s ease-out forwards;
-  }
-
-  .para-enter-from {
-    /*opacity: 0;*/
-    /*transform: translateY(-30px);*/
-  }
-
-  .para-enter-active {
-    /*transition: all 0.3s ease-out;*/
-    animation: slide-scale 0.3s ease-out;
-  }
-
-  .para-enter-to {
-    /*opacity: 1;*/
-    /*transform: translateY(0);*/
-  }
-
-  .para-leave-from {
-    /*opacity: 1;*/
-    /*transform: translateY(0);*/
-  }
-
-  .para-leave-active {
-    /*transition: all 0.3s ease-out;*/
-    animation: slide-scale 0.3s ease-out;
-  }
-
-  .para-leave-to {
-    /*opacity: 0;*/
-    /*transform: translateY(30px);*/
   }
 
   .fade-button-enter-from,
